e6526c9bf018bf944773bb53b18284f05c2ed3c6
mihailkudymov <mike.kudim@gmail.com>
2023-02-17 16:58:09 +0200

    Добавил функцию titilestr

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upstr(user_string):
     """функция делает возвращает строку заглавными буквами"""
     return user_string.upper()
+
+def titlestr(user_string):
+    """Функция возвращает первую букву каждого слова заглавной"""
+    return user_string.title()
